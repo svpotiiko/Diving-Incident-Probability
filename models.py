3a6ca8d7bc6dc2fbc67c2a308cecd4f717975256
Veronika Potiiko <svpotiiko@gmail.com>
2025-10-03 17:29:27 +0300

    Add reg results

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import statsmodels.formula.api as smf
 # Load dataset
 df = pd.read_csv("diving_synthetic_profiles.csv")
 
-# --- Logistic regression (Logit)
+# Logistic regression (Logit)
 logit_model = smf.logit(
     "Incident ~ MaxDepth_m + BottomTime_min + AscentRate_m_per_min + SafetyStop + NDL_Violation",
     data=df
@@ -13,10 +13,15 @@ logit_model = smf.logit(
 
 print(logit_model.summary())
 
-# --- Probit regression
+# Probit regression
 probit_model = smf.probit(
     "Incident ~ MaxDepth_m + BottomTime_min + AscentRate_m_per_min + SafetyStop + NDL_Violation",
     data=df
 ).fit()
 
-print(probit_model.summary())
+# Save output as HTML file
+with open("regression_results.html", "w") as f:
+    f.write(logit_model.summary().as_html())
+    f.write("<br><br>")
+    f.write(probit_model.summary().as_html())
+
